82f86c868d2ab4421087d577d89ec7febd8d9489
Harshita Daswani <87665916+harshitadaswani@users.noreply.github.com>
2021-08-11 02:51:03 +0530

    main file
    basics of function (question 07 solved)

---
+++ ex07/index.js
@@ -0,0 +1,24 @@
+// ex07: function to add 2 numbers
+
+// declaration of add function
+function add(n1 , n2)
+{
+	var sum = n1 + n2;
+	return sum;
+}
+
+// calling the function
+console.log(add(5,6));
+
+// extra
+
+// declaration of product function
+function prod(m1 , m2)
+{
+	var mul = m1 * m2;
+	return mul;
+}
+
+// calling the function
+result = prod(5,6);
+console.log(result);
